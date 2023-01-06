6196b2e207084b9ed9328d43e410eac9a4fae36a
hedi <hedihamida98@gmail.com>
2023-01-06 23:01:03 +0100

    setup

---
+++ module3_task1/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+if ! [ -x "$(command -v hugo)" ]; then
+	sudo apt-get update
+  	sudo apt-get install hugo
+fi
+
+if ! [ -x "$(command -v make)" ]; then
+  
+  sudo apt-get update
+  sudo apt-get install make
+fi
