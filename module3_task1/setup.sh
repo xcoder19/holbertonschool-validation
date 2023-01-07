c55eb6314fe8acb5eb195ea3cd28f4d467c30c2b
hedi <hedihamida98@gmail.com>
2023-01-07 09:58:27 +0100

    set

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if ! [ -x "$(command -v hugo)" ]; then
 	sudo apt-get update
-  	sudo apt-get install hugo
+  	sudo apt-get install hugo=0.84.0
 fi
 
 if ! [ -x "$(command -v make)" ]; then
