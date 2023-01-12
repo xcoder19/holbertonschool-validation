a70fa816559d5674d7b15270e33949571f0968e6
hedi <hedihamida98@gmail.com>
2023-01-12 12:54:58 +0100

    set

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -2,8 +2,7 @@
 if ! [ -x "$(command -v hugo)" ]; then
 	apt-get update
 	apt install zip unzip
-	apt install nodejs
-	
+	apt-get install -y nodejs
 	npm install -g markdownlint-cli
   	curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
 	apt install ./hugo.deb
