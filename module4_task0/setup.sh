55e9a655e4d6dd7bc5f2bdb7b9773efdc4ad04fb
hedi <hedihamida98@gmail.com>
2023-01-12 12:46:38 +0100

    set

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -2,7 +2,7 @@
 if ! [ -x "$(command -v hugo)" ]; then
 	apt-get update
 	apt install zip unzip
-	curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
+	curl -fsSL https://deb.nodesource.com/setup_18.x | -E bash - &&\
 	apt-get install -y nodejs
 	npm install -g markdownlint-cli
   	curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
