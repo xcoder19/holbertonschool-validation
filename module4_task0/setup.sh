b1b84fd8ec0d9d24f7bbadc02a27cf3d4cf382c0
hedi <hedihamida98@gmail.com>
2023-01-13 09:01:42 +0100

    set

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -5,7 +5,7 @@
 	npm install -g markdownlint-cli -y
 	curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
 	sudo apt install ./hugo.deb
-	
+	sudo apt-get remove golang-go
 	
 
   
