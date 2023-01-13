0bcc7bc6f5423cc26131ce7c7bb919d7a826d56c
hedi <hedihamida98@gmail.com>
2023-01-13 08:35:18 +0100

    set

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
-if ! [ -x "$(command -v hugo)" ]; then
 	apt-get update
-	apt install zip unzip
-	
-	
-	
-  	curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+	apt-get install -y make
+	apt install -y zip unzip
+	npm install -g markdownlint-cli -y
+	curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
 	apt install ./hugo.deb
-	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.50.1
-fi
+	
+	
 
-if ! [ -x "$(command -v make)" ]; then
   
-  sudo apt-get update
-  sudo apt-get install make
-fi
+
+
