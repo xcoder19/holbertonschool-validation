e788dd47198e89a577aaf2a082e74616e1f6ec5f
hedi <hedihamida98@gmail.com>
2023-01-13 08:36:13 +0100

    set

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-	apt-get update
-	apt-get install -y make
-	apt install -y zip unzip
+	sudo apt-get update
+	sudo apt-get install -y make
+	sudo apt install -y zip unzip
 	npm install -g markdownlint-cli -y
 	curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
 	apt install ./hugo.deb
