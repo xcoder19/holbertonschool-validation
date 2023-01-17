4b26676f89072b5a7015e3959e1fba390074ce37
hedi <hedihamida98@gmail.com>
2023-01-17 20:43:36 +0100

    set

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y hugo make
-  	 
+
+apt-get update && apt-get install -y make hugo
+
+
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+apt-get install ./hugo.deb
+
 make build
